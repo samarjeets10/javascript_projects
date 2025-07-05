8f6f9e4feddadc0a8a30fe35b5282bcd9f12ffb5
Samarjeet <samarsabale1021@gmail.com>
2025-07-05 14:44:55 +0530

    Improve dialog functionality and styles; add cancel button IDs and enhance dialog visibility

--- Note_Taking_App/script.js
+++ Note_Taking_App/script.js
@@ -1,1 +1,18 @@
 
+const addNoteBtn = document.getElementById('addNoteBtn');
+const cancleBtn = document.getElementById('cancleBtn');
+const crossBtn = document.getElementById('cancle');
+const dialogBox = document.querySelector('.dislog-box');
+
+
+addNoteBtn.addEventListener('click', () => {
+    dialogBox.classList.add('dialogActive');
+});
+
+crossBtn.addEventListener('click', exitTask);
+
+cancleBtn.addEventListener('click', exitTask);
+
+function exitTask() {
+    dialogBox.classList.remove('dialogActive');
+}
