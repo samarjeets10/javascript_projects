c7f092b109a6b8f0f28a48eb67542c19ee4185eb
Samarjeet <samarsabale1021@gmail.com>
2025-07-08 17:10:14 +0530

    Refactor toggle button for dark/light mode; update styles and improve accessibility

--- CSS_Button_component_library/script.js
+++ CSS_Button_component_library/script.js
@@ -2,11 +2,8 @@
 // Dark and Light Mode Btn :
 
 const modeBtn = document.querySelector('.mode-btn');
-const thumbBtn = document.querySelector('.thumb');
-const bodyBG = document.querySelector('body');
 
 modeBtn.addEventListener('click', () => {
-    thumbBtn.classList.toggle('mode-active');
-    bodyBG.classList.toggle('dark-Mode');
-});
-
+    modeBtn.classList.toggle('active');
+    document.body.classList.toggle('dark-Mode');
+})
