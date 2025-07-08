b555e6d9347ce9e71aecaf42d2b8ee2c7659f779
Samarjeet <samarsabale1021@gmail.com>
2025-07-08 21:13:05 +0530

    Add dark/light mode toggle functionality and like button feature; enhance UI elements and local storage integration

--- CSS_Button_component_library/script.js
+++ CSS_Button_component_library/script.js
@@ -2,8 +2,77 @@
 // Dark and Light Mode Btn :
 
 const modeBtn = document.querySelector('.mode-btn');
+let darkModeStatus = localStorage.getItem("darkModeStatus") === "true";
+
+if (darkModeStatus === true) {
+    darkModeOn();
+}
 
 modeBtn.addEventListener('click', () => {
-    modeBtn.classList.toggle('active');
-    document.body.classList.toggle('dark-Mode');
-})
+    darkModeStatus = !darkModeStatus;
+    console.log(darkModeStatus);
+
+    if (darkModeStatus === true) {
+        darkModeOn();
+    } else {
+        darkModeOff();
+    }
+
+    localStorage.setItem("darkModeStatus", darkModeStatus);
+});
+
+
+function darkModeOn () {
+    modeBtn.classList.add('active');
+    document.body.classList.add('dark-Mode');
+}
+
+function darkModeOff () {
+    modeBtn.classList.remove('active');
+    document.body.classList.remove('dark-Mode');
+};
+
+
+// Like and Unlike Button :
+
+
+const countLike = document.getElementById('like-count');
+const likeBtn = document.getElementById('likeBtn');
+
+let isActive = localStorage.getItem("likeStatus") === "true";
+let likeCount = localStorage.getItem('likeCount') || 0;
+
+if (isActive === true) {
+    like();
+    countLike.textContent = `${likeCount}`;
+}
+
+likeBtn.addEventListener('click', () => {
+    isActive = !isActive;
+    console.log(isActive);
+
+    if (isActive) {
+        like();
+    } else {
+        dislike();
+    }
+
+    countLike.textContent = `${likeCount}`;
+
+    //  Save to Local Storage :
+
+    localStorage.setItem("likeStatus", isActive);
+    localStorage.setItem("likeCount", likeCount);
+});
+
+function like () {
+    likeBtn.classList.add('active');
+    likeCount++;
+}
+
+function dislike () {
+    likeBtn.classList.remove('active');
+    likeCount--;
+}
+
+
