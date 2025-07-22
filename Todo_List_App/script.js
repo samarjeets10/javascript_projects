e268c3af143eefeb6b9f558e71b6a5eff03cac67
Samarjeet <samarsabale1021@gmail.com>
2025-07-22 18:30:52 +0530

    Implement task addition functionality and enhance UI for task items

--- Todo_List_App/script.js
+++ Todo_List_App/script.js
@@ -2,7 +2,8 @@
 const inputTask = document.getElementById('input-task');
 const inputDate = document.getElementById('input-date');
 const addTaskBtn = document.getElementById('add-task-btn');
-const form = document.querySelector('form');
+const taskContent = document.querySelector('.task-content');
+const defaultCard = document.querySelector('.default-task');
 
 addTaskBtn.addEventListener('click', (task) => {
     task.preventDefault();
@@ -21,8 +22,40 @@ function vallidation (userInput, userDate) {
         alert("Please enter task and date");
     } else {
         addTask(userInput, userDate);
+        defaultCardRemove();
     }
 }
+
+
+function defaultCardRemove () {
+
+    defaultCard.classList.add('active');
+}
+
+function addTask(userInput, userDate) {
+
+    const containerEle = document.createElement('div');
+    containerEle.setAttribute('class', 'task-item');
+
+    containerEle.innerHTML = `
+
+        <div class="text-content">
+            <h3 class="inputTask">${userInput}</h3>
+            <span class="inputDate">${userDate}</span>
+        </div>
+        <button class="delete-btn">Delete</button>
+    `;
+
+    taskContent.appendChild(containerEle);
+    inputCleanUp();
+}
+
+function inputCleanUp () {
+    inputTask.value = "";
+    inputDate.value = "";
+}
+
+
 
 
 
